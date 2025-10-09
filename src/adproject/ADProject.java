6b5235858c8b26ba5c3ca2c993a55fdd62c168dc
BlueHunter89053 <joshuabonzet4@gmail.com>
2025-10-09 08:57:19 +0200

    I used a JOptionPane method.

--- src/adproject/ADProject.java
+++ src/adproject/ADProject.java
@@ -4,6 +4,8 @@
  */
 package adproject;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author joshua
@@ -15,7 +17,8 @@ public class ADProject {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World!");
+        String name = JOptionPane.showInputDialog("What is your name?");
+	System.out.println(name);
     }
     
 }
