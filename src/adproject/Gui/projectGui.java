19f1287cd7441e0d93806d7e01ec4077de11122d
Joshua <joshuabonzet4@gmail.com>
2025-10-10 23:13:59 +0200

    Completed the administrator frame.

--- src/adproject/Gui/projectGui.java
+++ src/adproject/Gui/projectGui.java
@@ -5,6 +5,7 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class projectGui {
+
     JButton btnLogin, btnClear, btnExit;
     JPanel pnlTop, pnlMiddle, pnlBottom;
     JLabel lblStudNum, lblPassword;
@@ -17,32 +18,39 @@ public class projectGui {
 
     public void setGui(JFrame frame) {
         frame.setLayout(new GridLayout(3, 1));
-        
+
         //Setting up the JPanels
         pnlTop = new JPanel();
         pnlMiddle = new JPanel();
         pnlBottom = new JPanel();
-        
+
         //Populating top JPanel
         lblStudNum = new JLabel("Student Number");
         tfName = new JTextField(14);
         pnlTop.add(lblStudNum);
         pnlTop.add(tfName);
-        
+
         //Populating the middle JPanel
         lblPassword = new JLabel("Password");
         pfPassword = new JPasswordField(10);
         pnlMiddle.add(lblPassword);
         pnlMiddle.add(pfPassword);
-        
+
         //Setting up JComboBox
         String[] roles = {"--Select--", "Student", "Staff"};
         cboRole = new JComboBox<>(roles);
         cboRole.setPreferredSize(new Dimension(80, 25));
         pnlMiddle.add(cboRole);
-        
+
         //Setting up the bottom JPanel
         btnLogin = new JButton("Login");
+        btnLogin.addActionListener(e -> {
+            String roleSelected = (String) cboRole.getSelectedItem();
+            if ("Staff".equals(roleSelected)) {
+                frame.setVisible(false);
+                adminFrame();
+            }
+        });
         btnClear = new JButton("Clear");
         btnClear.addActionListener(e -> { //used lambda expression to add functionality to the button
             tfName.setText("");
@@ -55,53 +63,130 @@ public class projectGui {
         pnlBottom.add(btnLogin);
         pnlBottom.add(btnClear);
         pnlBottom.add(btnExit);
-        
+
         //Add panels to frame
         frame.add(pnlTop);
         frame.add(pnlMiddle);
         frame.add(pnlBottom);
     }
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == btnLogin) {
-            String roleSelected = (String) cboRole.getSelectedItem();
-            if ("Staff".equals(roleSelected)) {
-                adminFrame();
-            }
-        }
+
+    public void adminFrame() {
+        //Create new frame
+        JFrame adminFrame = new JFrame("Administrator");
+        adminFrame.setSize(400, 400);
+        adminFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        adminFrame.setLocationRelativeTo(null);
+
+        //Create tabbed pane
+        JTabbedPane tab = new JTabbedPane();
+
+        //Main panel object
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        //gridPanel object
+        JPanel gridPanel = new JPanel(new GridLayout(4, 2, 5, 5));
+        gridPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        //Components of gridPanel
+        JLabel lblCourseName = new JLabel("Course name:");
+        JTextField tfCourseName = new JTextField(10);
+        tfCourseName.setPreferredSize(new Dimension(25, 25));
+        JLabel lblCourseCode = new JLabel("Course code:");
+        JTextField tfCourseCode = new JTextField(10);
+        tfCourseCode.setPreferredSize(new Dimension(25, 25));
+        //flowPanel object used for JButtons
+        JPanel flowPanel = new JPanel(new FlowLayout());
+        //Submit button functionality
+        JButton btnSubmit = new JButton("Submit");
+        btnSubmit.addActionListener(e -> {
+            //This code must send the data to the database. 
+        });
+        //Clear button functionality
+        JButton btnClear = new JButton("Clear");
+        btnClear.addActionListener(e -> {
+            tfCourseName.setText("");
+            tfCourseCode.setText("");
+        });
+        //Exit button functionality
+        JButton btnExit = new JButton("Exit");
+        btnExit.addActionListener(e -> {
+            System.exit(0);
+        });
+        //Add components to gridPanel
+        gridPanel.add(lblCourseName);
+        gridPanel.add(tfCourseName);
+        gridPanel.add(lblCourseCode);
+        gridPanel.add(tfCourseCode);
+        //Add components to flowPanel
+        flowPanel.add(btnSubmit);
+        flowPanel.add(btnClear);
+        flowPanel.add(btnExit);
+        //Add gridPanel & flowPanel to mainPanel
+        mainPanel.add(gridPanel, BorderLayout.CENTER);
+        mainPanel.add(flowPanel, BorderLayout.SOUTH);
+
+        //Create the student panel object
+        JPanel mainPanel2 = new JPanel(new BorderLayout());
+        //Components of student panel
+        JPanel gridPanel2 = new JPanel(new GridLayout(4, 2, 10, 10));
+        JLabel lblName = new JLabel("Name:");
+        JTextField tfName = new JTextField(15);
+        JLabel lblSname = new JLabel("Surname:");
+        JTextField tfSname = new JTextField(15);
+        JLabel lblEmail = new JLabel("Email:");
+        JTextField tfEmail = new JTextField(15);
+        JLabel lblStudNum = new JLabel("Student Number:");
+        JTextField tfStudNum = new JTextField(15);
+        //Flow panel object
+        JPanel flowPanel2 = new JPanel(new FlowLayout());
+        //Submit button functionality
+        JButton btnSubmit2 = new JButton("Submit");
+        btnSubmit2.addActionListener(e -> {
+            //Add code that adds the student details to the database
+        });
+        //Clear button functionality
+        JButton btnClear2 = new JButton("Clear");
+        btnClear2.addActionListener(e -> {
+            tfName.setText("");
+            tfSname.setText("");
+            tfEmail.setText("");
+            tfStudNum.setText("");
+        });
+        //Exit button functionality
+        JButton btnExit2 = new JButton("Exit");
+        btnExit2.addActionListener(e -> {
+            System.exit(0);
+        });
+        //Add components to grid panel
+        gridPanel2.add(lblName);
+        gridPanel2.add(tfName);
+        gridPanel2.add(lblSname);
+        gridPanel2.add(tfSname);
+        gridPanel2.add(lblEmail);
+        gridPanel2.add(tfEmail);
+        gridPanel2.add(lblStudNum);
+        gridPanel2.add(tfStudNum);
+        //Add components to flow panel
+        flowPanel2.add(btnSubmit2);
+        flowPanel2.add(btnClear2);
+        flowPanel2.add(btnExit2);
+        //Add flow & grid panel to main panel
+        mainPanel2.add(gridPanel2, BorderLayout.CENTER);
+        mainPanel2.add(flowPanel2, BorderLayout.SOUTH);
+        //Add the main panel to the tab
+        tab.addTab("Add Course", mainPanel);
+        tab.addTab("Add Student", mainPanel2);
+        //Add panels to frame
+        adminFrame.add(tab);
+        //Make frame visible
+        adminFrame.setVisible(true);
     }
     
-    public void adminFrame() {
-        //this.setVisible(false);
-        
+    public void studentFrame() {
         //Create new frame
         JFrame adminFrame = new JFrame("Administrator");
         adminFrame.setSize(400, 400);
         adminFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         adminFrame.setLocationRelativeTo(null);
         
-        //Create tabbed pane
-        JTabbedPane tab = new JTabbedPane();
-        
-        //Create the course panel
-        JPanel mainPanel = new JPanel(new BorderLayout());
-        
-        JPanel gridPanel = new JPanel(new GridLayout(2, 2, 10, 10));
-        
-        gridPanel.add(new JLabel("Course name:"));
-        gridPanel.add(new JTextField(15));
-        gridPanel.add(new JLabel("Course code:"));
-        gridPanel.add(new JTextField(15));
-        
-        JPanel flowPanel = new JPanel(new FlowLayout());
-        flowPanel.add(new JButton("Submit"));
-        flowPanel.add(new JButton("Clear"));
-        flowPanel.add(new JButton("Exit"));
-        
-        mainPanel.add(gridPanel);
-        mainPanel.add(flowPanel);
-        
-        
         
     }
-    
 }
