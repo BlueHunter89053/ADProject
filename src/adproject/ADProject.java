2babcd4f56f09f19dcdb6afdb175e0dcdad46dba
Joshua <joshuabonzet4@gmail.com>
2025-10-11 21:42:00 +0200

    Added nothing important.

--- src/adproject/ADProject.java
+++ src/adproject/ADProject.java
@@ -3,8 +3,6 @@ package adproject;
 import adproject.Gui.projectGui;
 import javax.swing.*;
 
-import javax.swing.JOptionPane;
-
 public class ADProject {
 
     public static void main(String[] args) {
@@ -15,7 +13,6 @@ public class ADProject {
         projectGui gui = new projectGui();
         gui.setGui(frame);
         frame.setVisible(true);
-
     }
 
 }
