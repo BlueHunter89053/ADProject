515510158f5458975a4edc5af924fc85dd433a4a
Joshua <joshuabonzet4@gmail.com>
2025-10-11 21:28:36 +0200

    Added the setVisible method to the end of the main method.

--- src/adproject/ADProject.java
+++ src/adproject/ADProject.java
@@ -1,20 +1,21 @@
 package adproject;
+
 import adproject.Gui.projectGui;
 import javax.swing.*;
 
 import javax.swing.JOptionPane;
 
 public class ADProject {
-	
+
     public static void main(String[] args) {
-	    JFrame frame = new JFrame("LOGIN");
-            frame.setSize(500, 200);
-	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    frame.setLocationRelativeTo(null);
-            projectGui gui = new projectGui();
-	    gui.setGui(frame);
-	    
-	    frame.setVisible(true);
+        JFrame frame = new JFrame("LOGIN");
+        frame.setSize(500, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        projectGui gui = new projectGui();
+        gui.setGui(frame);
+        frame.setVisible(true);
+
     }
-    
+
 }
