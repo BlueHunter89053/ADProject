0c4a161b95a83ac8133231547424d17a22848680
Joshua <joshuabonzet4@gmail.com>
2025-10-12 23:01:40 +0200

    Set up setters and getters for course class

--- src/adproject/Domain/Student.java
+++ src/adproject/Domain/Student.java
@@ -9,5 +9,44 @@ package adproject.Domain;
  * @author Albert
  */
 public class Student {
-	
+    public String Name, Sname, Email;
+    public int StudNum;
+
+    public Student() {
+
+    }
+
+    //Setters
+    public void setName() {
+        this.Name = Name;
+    }
+
+    public void setSname() {
+        this.Sname = Sname;
+    }
+
+    public void setEmail() {
+        this.Email = Email;
+    }
+
+    public void setStudNum() {
+        this.StudNum = StudNum;
+    }
+
+    //Getters
+    public String getName(String Name) {
+        return Name;
+    }
+
+    public String getSname(String Sname) {
+        return Sname;
+    }
+
+    public String getEmail(String Email) {
+        return Email;
+    }
+
+    public String getStudNum(String StudNum) {
+        return StudNum;
+    }
 }
